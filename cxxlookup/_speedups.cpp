e53c07de5ce5a4396a8d88cf4b306456cd22318f
chys <admin@chys.info>
2022-06-02 23:20:22 +0800

    Update copyright and license info

--- cxxlookup/_speedups.cpp
+++ cxxlookup/_speedups.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014, chys <admin@CHYS.INFO>
+Copyright (c) 2014-2022, chys <admin@CHYS.INFO>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
